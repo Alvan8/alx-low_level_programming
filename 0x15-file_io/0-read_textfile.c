0877b30d8ec65fb00d72086ddaf0840dd6842597
Alvan8 <alvanbroon8@gmail.com>
2023-05-08 09:54:03 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,43 +12,23 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor;
-	ssize_t bytes_read;
-	char *buffer;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
-	{
-		close(file_descriptor);
-		return (0);
-	}
-
-	bytes_read = read(file_descriptor, buffer, letters);
-	if (bytes_read == -1)
-	{
-		close(file_descriptor);
-		free(buffer);
-		return (0);
-	}
-
-	buffer[bytes_read] = '\0'; /* Null-terminate the buffer */
-
-	if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
-	{
-		close(file_descriptor);
-		free(buffer);
-		return (0);
-	}
-
-	close(file_descriptor);
-	free(buffer);
-
-	return (bytes_read);
+	free(buf);
+	close(fd);
+	return (w);
 }
+
+
+
+
