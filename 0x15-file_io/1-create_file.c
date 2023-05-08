f0f84e29897970dfc4d5276df16c7b15f3eef43e
Alvan8 <alvanbroon8@gmail.com>
2023-05-08 10:23:11 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,42 +9,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, bytes_written;
-	mode_t permissions = S_IRUSR | S_IWUSR;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permissions);
-	if (file_descriptor == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		bytes_written = write(file_descriptor, text_content, _strlen(text_content));
-		if (bytes_written == -1)
-		{
-			close(file_descriptor);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	close(file_descriptor);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
 	return (1);
 }
 
-/**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
- *
- * Return: The length of the string.
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int length = 0;
 
-	while (str[length] != '\0')
-		length++;
 
-	return (length);
-}
+
