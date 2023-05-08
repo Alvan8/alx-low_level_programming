8b280db310d8195c51aed7cb7fb62f9beae39de4
Alvan8 <alvanbroon8@gmail.com>
2023-05-08 09:50:28 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads then  prints a text file it to POSIX standard output.
+ * @filename: The name of the file to be read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	ssize_t bytes_read;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
+
+	buffer[bytes_read] = '\0'; /* Null-terminate the buffer */
+
+	if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
+
+	close(file_descriptor);
+	free(buffer);
+
+	return (bytes_read);
+}
