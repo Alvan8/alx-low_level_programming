892696eaba2251d3c9512391f637f02990cceb59
Alvan8 <alvanbroon8@gmail.com>
2023-05-08 10:09:48 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file then print sto POSIX STDOUT.
+ * read_textfile- Read text file print to STDOUT.
  * @filename: text file being read
  * @letters: number of letters to be read
  * Return: w- actual number of bytes read and printed
@@ -26,5 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 	return (w);
 }
+
+
 
 
