3e230b907e827d6bab126487134e87778929ed6c
Alvan8 <alvanbroon8@gmail.com>
2023-05-08 10:40:44 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,21 +1,19 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-#define BUFFER_SIZE 1024
 
 char *create_buffer(char *file);
 void close_file(int fd);
 
-
 /**
- * main - Copies the content of a file to another file.
- * @argc: The argument count.
- * @argv: The argument vector.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: 0 on success, or the corresponding error code.
+ * Return: A pointer to the newly-allocated buffer.
  */
-int main(int argc, char **argv)
-char *buffer;
+char *create_buffer(char *file)
+{
+	char *buffer;
 
 	buffer = malloc(sizeof(char) * 1024);
 
@@ -103,11 +101,5 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
-
-
-
-
-
 
 
