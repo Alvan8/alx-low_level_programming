c9affed03f96b6e9a3832fbd12a85a956d59fa72
Alvan8 <alvanbroon8@gmail.com>
2023-05-08 10:29:48 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,16 +20,17 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(fd);
+	close(o);
 
 	return (1);
 }
+
 
 
 
